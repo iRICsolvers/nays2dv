2abda057e6c32b529c4acc057e7c8545dcad9dc9
starfieldsKt <keita.hoshino.1001@gmail.com>
2022-07-05 17:32:34 +0900

    make.batの内容を修正

--- make.bat
+++ make.bat
@@ -6,8 +6,8 @@ rem ----------------------------------------------------------------------
 rem ifort compile
 rem ----------------------------------------------------------------------
 ifort .\src\iric.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
-ifort  /F100000000 /f77rtl /fixed /extend-source:132 /MD /c .\src\nays2d+.f
-ifort *.obj .\lib\iriclib.lib -o ".\install\nays2d+.exe"
+ifort .\src\nays2dv.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort *.obj .\lib\iriclib.lib -o ".\install\nays2dv.exe"
 
 del *.obj
 del *.mod
